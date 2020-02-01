766dabcde8e571373758e4dd1b67e075ef71e95d
Mike Scott <scott.michael3@gmail.com>
2020-02-01 18:23:19 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method here
 
 def say_hello("name", language = "ruby programmer")
-  puts "hello #{name} !" 
+  puts "hello #{name} !  I hear you're a great #{language}!" 
 end
